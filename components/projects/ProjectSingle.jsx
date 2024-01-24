55d9ecae1a84ccc5bdfbf3af0fa974edf2ac113f
DonJoel <jt3bk@virginia.edu>
2024-01-24 18:17:06 -0500

    fixing image size

--- components/projects/ProjectSingle.jsx
+++ components/projects/ProjectSingle.jsx
@@ -16,19 +16,23 @@ const ProjectSingle = props => {
       }}
     >
       <Link href={props.link} aria-label="Single Project" passHref>
-        <div className="rounded-xl shadow-lg hover:shadow-xl h-30 cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark ">
-          <Image
-            src={props.img}
-            className="rounded-t-xl border-none"
-            alt="Single Project"
-            layout="responsive"
-            width={100}
-            height={90}
-          />
-
+        <Image
+          src={props.img}
+          style={{
+            position: "relative",
+            width: "100%",
+            height: "80%"
+          }}
+          className="rounded-t-xl border-none"
+          alt="Single Project"
+          width={500}
+          height={400}
+        />
+        <div className="rounded-b-xl  shadow-lg hover:shadow-xl h-30 cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark">
           <div>
-            <div className="text-center px-4 py-6">
-              <p className="font-general-medium text-xl md:text-2xl text-ternary-dark dark:text-ternary-light mb-2">
+            <div className="text-center px-4 py-6 ">
+              <div></div>
+              <p className="font-general-medium text-xl md:text-2xl text-ternary-dark dark:text-ternary-light mb-2 ">
                 {props.title}
               </p>
               <span className="text-lg text-ternary-dark dark:text-ternary-light">
