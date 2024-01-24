09de126c1bfe06bf98500091884afd4d686c376d
DonJoel <jt3bk@virginia.edu>
2024-01-24 18:33:58 -0500

    adding in dark/light button

--- components/shared/AppHeader.jsx
+++ components/shared/AppHeader.jsx
@@ -63,19 +63,7 @@ function AppHeader() {
           id="menu-links"
           className=" items-center px-4 sm:px-0 flex-grow-0 ml-auto flex"
         ></div>
-        {/* Theme switcher small screen */}
-        {/* <div
-          id="theme-switcher-small-screen"
-          onClick={() => setTheme(activeTheme)}
-          aria-label="Theme Switcher"
-          className="block sm:hidden ml-0 bg-primary-light dark:bg-ternary-dark p-3 shadow-sm rounded-xl w-11 cursor-pointer flex-grow-0 ml-auto"
-        >
-          {activeTheme === "dark" ? (
-            <FiMoon className="text-ternary-dark hover:text-gray-400 dark:text-ternary-light dark:hover:text-primary-light text-xl" />
-          ) : (
-            <FiSun className="text-gray-200 hover:text-gray-50 text-xl" />
-          )}
-        </div> */}
+
         {/* Header links small screen */}
         <div
           className={
@@ -86,27 +74,39 @@ function AppHeader() {
         >
           <div className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark">
             <DropdownButton />
+            {/* Theme switcher small screen */}
+            <div
+              id="theme-switcher-small-screen"
+              onClick={() => setTheme(activeTheme)}
+              aria-label="Theme Switcher"
+              className="block sm:hidden ml-0 bg-primary-light dark:bg-ternary-dark p-3 shadow-sm rounded-xl w-11 cursor-pointer flex-grow-0 ml-auto"
+            >
+              {activeTheme === "dark" ? (
+                <FiMoon className="text-ternary-dark hover:text-gray-400 dark:text-ternary-light dark:hover:text-primary-light text-xl" />
+              ) : (
+                <FiSun className="text-gray-200 hover:text-gray-50 text-xl" />
+              )}
+            </div>
           </div>
         </div>
 
         {/* Header links large screen */}
         <div className="font-general-medium hidden m-0 lg:ml-4 mt-5 lg:mt-3 lg:flex p-5 lg:p-0  shadow-lg lg:shadow-none">
           <DropdownButton />
-        </div>
-
-        {/* Theme switcher large screen */}
-        {/* <div
+          {/* Theme switcher large screen */}
+          <div
             id="theme-switcher-large-screen"
             onClick={() => setTheme(activeTheme)}
             aria-label="Theme Switcher"
-            className=" bg-primary-light dark:bg-ternary-dark p-3 w-11 h-11 hidden sm:block rounded-xl cursor-pointer"
+            className=" bg-primary-light dark:bg-ternary-dark p-3 w-11 h-11 hidden sm:block rounded-xl cursor-pointer ml-5"
           >
             {activeTheme === "dark" ? (
               <FiMoon className="text-ternary-dark hover:text-gray-400 dark:text-ternary-light dark:hover:text-primary-light text-xl" />
             ) : (
               <FiSun className="text-gray-200 hover:text-gray-50 text-xl" />
             )}
-          </div> */}
+          </div>
+        </div>
       </div>
     </motion.nav>
   );
