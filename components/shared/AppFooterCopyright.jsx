baee4e462212780b3d1fd5ec629e3555cf5a7642
DonJoel <jt3bk@virginia.edu>
2024-02-02 20:24:02 -0500

    editing footer

--- components/shared/AppFooterCopyright.jsx
+++ components/shared/AppFooterCopyright.jsx
@@ -1,18 +1,18 @@
 function AppFooterCopyright() {
-	return (
-		<div className="font-general-regular flex justify-center items-center text-center">
-			<div className="text-lg text-ternary-dark dark:text-ternary-light">
-				&copy; {new Date().getFullYear()}
-				<a
-					href=""
-					target="__blank"
-					className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					Joel Tavarez
-				</a>
-			</div>
-		</div>
-	);
+  return (
+    <div className="font-general-regular flex justify-center items-center text-center">
+      <div className="text-lg text-ternary-dark dark:text-ternary-light">
+        <a
+          href=""
+          target="__blank"
+          className="text-secondary-dark dark:text-secondary-light font-medium  hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+        >
+          Made with Next.js and Tailwind by Joel Tavarez
+        </a>
+        <a> &copy; {new Date().getFullYear()}</a>
+      </div>
+    </div>
+  );
 }
 
 export default AppFooterCopyright;
