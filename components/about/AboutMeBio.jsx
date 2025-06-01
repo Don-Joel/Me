583bb1e4ee2b366aa3ba8bcc47d637790f0512b8
Joel Tavarez <joeltavarez@Joels-MacBook-Pro.local>
2025-05-31 22:32:17 -0400

    edit bio

--- components/about/AboutMeBio.jsx
+++ components/about/AboutMeBio.jsx
@@ -77,7 +77,7 @@ function AboutMeBio() {
           In addition to my product role, I take part in Target&#39;s performance guild where I pursue
           my interests in technical performance and design. In my leisure, I like to keep myself up to date with the latest tech and
           economics news, primarly focusing on AI and macroeconomics. I also have a deep passion for systems engineering and thinking 
-          which I integrate in my work and everyday life. Besides my technical passions, I enjoy a fit lifestyle and hold deep
+          which I integrate in my work and everyday life. Besides my technical passions, I enjoy a fitness lifestyle and have
           interests in endocrinology and nutrition. I also enjoy
           reading and having new experiences that could lead me to new
           outlooks on life. Ultimately, with my career, I hope to create
