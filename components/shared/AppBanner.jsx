9ea2a1bf20931f84f5e0cd2d9fa9d5bd448a6b20
Joel Tavarez <joeltavarez@Joels-MacBook-Pro.local>
2025-05-31 22:33:12 -0400

    edit bio

--- components/shared/AppBanner.jsx
+++ components/shared/AppBanner.jsx
@@ -32,7 +32,7 @@ function AppBanner() {
           }}
           className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
         >
-          a product engineer with experience developing microservices in JVM
+          product engineer with experience developing microservices in JVM
           languages and large web applications written with React
         </motion.p>
         <motion.div
