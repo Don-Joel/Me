7e9623f9d1447440b3e87bdabb505bfb889fd05b
Joel Tavarez <joeltavarez@Joels-MacBook-Pro.local>
2025-08-13 23:59:25 -0400

    changing font for contact me

--- components/shared/DropdownButton.jsx
+++ components/shared/DropdownButton.jsx
@@ -17,7 +17,7 @@ const DropdownButton = () => {
     <div className="relative inline-block">
       <button
         onClick={toggleDropdown}
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-general-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Contact Me
       </button>
