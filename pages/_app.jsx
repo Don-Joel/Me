fec291dadaf6f46ded75a3faa3d9d8abfbff5d8c
Joel Tavarez <joeltavarez@Joels-MacBook-Pro.local>
2025-12-05 23:01:56 -0600

    defaulting user to dark mode

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,18 +1,33 @@
 import "../styles/globals.css";
 import { AnimatePresence } from "framer-motion";
+import Script from "next/script";
 import DefaultLayout from "../components/layout/DefaultLayout";
 import UseScrollToTop from "../hooks/useScrollToTop";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <AnimatePresence>
-      <div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
-        <DefaultLayout>
-          <Component {...pageProps} />
-        </DefaultLayout>
-        <UseScrollToTop />
-      </div>
-    </AnimatePresence>
+    <>
+      <Script
+        id="theme-init"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function() {
+              const theme = localStorage.getItem('theme') || 'dark';
+              document.documentElement.classList.add(theme);
+            })();
+          `,
+        }}
+      />
+      <AnimatePresence>
+        <div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
+          <DefaultLayout>
+            <Component {...pageProps} />
+          </DefaultLayout>
+          <UseScrollToTop />
+        </div>
+      </AnimatePresence>
+    </>
   );
 }
 
